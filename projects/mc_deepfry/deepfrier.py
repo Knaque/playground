ca07c23e6d794fd8da1b4829903edb4ab3ca98ff
Reilly Moore <williammoore6105@gmail.com>
2020-02-12 10:28:45 -0600

    It works, I think.

--- projects/mc_deepfry/deepfrier.py
+++ projects/mc_deepfry/deepfrier.py
@@ -1,41 +1,73 @@
-"""This program deepfries every image in the default resource pack."""
+"""This program deepfries every image in the provided resource pack."""
 
-
-from PIL import Image, ImageEnhance
+try:
+    from PIL import Image, ImageEnhance
+except ImportError:
+    print("Missing required module 'Pillow'")
+    print("python3.7 -m pip install Pillow")
 import os
 
 
+# ============================= Load settings.txt =============================
+print("Attempting to load settings...")
+
+# These are the default settings used by this script.
 default_settings = """# Name of the pack (folder) you're using as input.
 input_pack = 'Default'
 
-# Should the input pack be overwritten (True) or a new folder made (False)?
-overwrite_input = False
+# Should a new pack be created? If false, the input pack will be overwritten
+new_pack = True
 
-# Name of the pack (folder) to create. Ignored if overwrite_input = True
-output_pack = 'Deepfried'"""
+# Name of the pack (folder) to create. Ignored if new_pack = False
+new_pack_name = 'Deepfried'
 
-with open('settings.txt', 'r') as data:
-    try:
-        settings = data.read()
-        exec(settings)
-    except Exception:
-        with open('settings.txt', 'w') as data:
-            data.write(default_settings)
-        with open('settings.txt', 'r') as data:
-            settings = data.read()
-            exec(settings)
+# NOTE: This text file is run as Python code by the script, so I claim no
+# responsibility for the outcome if you attempt to modify this file with
+# new code or input invalid info."""
 
-if ('input_pack' in globals() and 'overwrite' in globals() and
-        'output_pack' in globals()):
-    pass
-else:
-    with open('settings.txt', 'w') as data:
-        data.write(default_settings)
+# Create empty vars to avoid linting errors & check existence
+input_pack = None
+new_pack = None
+new_pack_name = None
+
+# Try and read settings.txt; if an error happens, restore to default.
+try:
     with open('settings.txt', 'r') as data:
         settings = data.read()
         exec(settings)
+except Exception:
+    print("An exception occured, so settings.txt is being reset.")
+    with open('settings.txt', 'w') as data:
+        data.write(default_settings)
+    print("The script will now abort so you can change your settings.")
+    quit()
+
+# If no errors occur, make sure that all the required variables exist
+if (input_pack is not None and new_pack is not None and
+        new_pack_name is not None):
+    # Ensure each variable is the correct type
+    if (type(input_pack) == str and type(new_pack) == bool and
+            type(new_pack_name) == str):
+        pass
+    else:
+        print("One or more variables was the wrong type, so settings.txt is",
+              "being reset.")
+        with open('settings.txt', 'w') as data:
+            data.write(default_settings)
+        print("The script will now abort so you can change your settings.")
+        quit()
+else:
+    print("One or more variables was missing, so settings.txt is being reset.")
+    with open('settings.txt', 'w') as data:
+        data.write(default_settings)
+    print("The script will now abort so you can change your settings.")
+    quit()
+
+print("Settings loaded successfully!")
+# =============================================================================
 
 
+# ========================= Define texture locations ==========================
 textures = '/assets/minecraft/textures/'
 subdirs = ['block/',
            'colormap/',
@@ -93,20 +125,58 @@ subdirs = ['block/',
            'map/',
            'misc/',
            'mob_effect/',
+
+           'models/',
            'models/armor/',
+
            'painting/',
            'particle/']
+# =============================================================================
 
-print("Working...")
-if not os.path.isdir(out_dir + textures):
-    os.mkdir(out_dir + textures)
+
+# ================== Create new resource pack if applicable ===================
+if new_pack is True:
+    print("Creating a new resource pack...")
+    # main folder
+    if not os.path.isdir(new_pack_name):
+        os.mkdir(new_pack_name)
+    # assets
+    if not os.path.isdir(new_pack_name + "/assets/"):
+        os.mkdir(new_pack_name + "/assets/")
+    # minecraft
+    if not os.path.isdir(new_pack_name + "/assets/minecraft/"):
+        os.mkdir(new_pack_name + "/assets/minecraft/")
+    # textures
+    if not os.path.isdir(new_pack_name + textures):
+        os.mkdir(new_pack_name + textures)
+    for subdir in subdirs:
+        if not os.path.isdir(new_pack_name + textures + subdir):
+            os.mkdir(new_pack_name + textures + subdir)
+
+    mcmeta = """{
+   "pack":{
+      "pack_format":5,
+      "description":"\u00A7cD e e p f r i e d"
+   }
+}"""
+    with open(new_pack_name + "/pack.mcmeta", 'w') as data:
+        data.write(mcmeta)
+
+    print(f"Created new pack '{new_pack_name}'")
+# =============================================================================
+
+
+# ========================== Do the image processing ==========================
+print("Creating new textures...")
+
 for subdir in subdirs:
-    for file in os.listdir(in_dir + textures + subdir):
+    for file in os.listdir(input_pack + textures + subdir):
         try:
-            img = Image.open(in_dir + textures + subdir + file)
+            img = Image.open(input_pack + textures + subdir + file)
             img = img.convert("RGBA")
             enh = ImageEnhance.Color(img)
-            enh.enhance(255).save(out_dir + textures + subdir + file)
+            enh.enhance(255).save(new_pack_name + textures + subdir + file)
         except IOError:
             pass
-print("Done!")
+
+print("All done!")
